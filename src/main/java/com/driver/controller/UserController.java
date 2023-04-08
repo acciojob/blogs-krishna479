f985338ca3ebcc6b6b02ba70b712a6458fb65937
krishna479 <krishankant.sharma@gmail.com>
2023-04-08 10:17:27 +0530

    first

--- src/main/java/com/driver/controller/UserController.java
+++ src/main/java/com/driver/controller/UserController.java
@@ -9,23 +9,30 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> createUser(@RequestParam String username, @RequestParam String password) {
-        // create a new user with given username and password
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
 
-    @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable int userId) {
-        // delete user using deleteById
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+    public class UserController {
+        @Autowired
+        UserService userService;
 
-    @PutMapping("/update")
-    public ResponseEntity<Void> updateUser(@RequestParam Integer id, @RequestParam String password) {
-        // update password of given user
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+        @PostMapping("/create")
+        public ResponseEntity<Void> createUser(@RequestParam String username, @RequestParam String password) {
+            // create a new user with given username and password
+            userService.createUser(username,password);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        @DeleteMapping("/delete/{userId}")
+        public ResponseEntity<Void> deleteUser(@PathVariable int userId) {
+            // delete user using deleteById
+            userService.deleteUser(userId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        @PutMapping("/update")
+        public ResponseEntity<Void> updateUser(@RequestParam Integer id, @RequestParam String password) {
+            // update password of given user
+            userService.updateUser(id,password);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
 }
